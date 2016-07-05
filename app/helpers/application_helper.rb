11304d0471d94bd681efb24037911d26dc86978b
Kenny Udovic <kennyudovic@gmail.com>
2016-07-05 15:52:14 -0400

    Added a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Handles the construction and returning of full title on a per-page basis,
+  def full_title(page_title = '')
+    base_title = "Adventure Space"
+    if page_title.empty?
+      return base_title
+    else
+      return page_title + " | " + base_title
+    end
+  end
+
 end
